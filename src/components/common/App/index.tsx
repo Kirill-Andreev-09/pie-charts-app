c3da3ed9ba041d1aaf1d47900cf65320ffe34bf7
Kirill <kirill_1131@mail.ru>
2022-06-21 18:29:40 +0300

    added routeUrl

--- src/components/common/App/index.tsx
+++ src/components/common/App/index.tsx
@@ -4,9 +4,11 @@ import {
   Route,
   Navigate,
   Routes,
+  BrowserRouter,
 } from "react-router-dom";
 import {Dashboard} from "../../../pages/Dashboard";
 import {SignIn} from "../../../pages/SignIn";
+import {routeUrl} from "../../../utils/constants";
 
 export type AppContextDataType = {
   token: string | null;
@@ -14,15 +16,20 @@ export type AppContextDataType = {
 
 export const AppContext = React.createContext<AppContextDataType | null>(null);
 
+const basepath: string = routeUrl === "" ? "/" : routeUrl;
+
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter basename={basepath}>
       <Routes>
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/" element={<Navigate replace to="/sign-in" />} />
+        <Route
+          path="/"
+          element={<Navigate replace to={`${routeUrl}/sign-in`} />}
+        />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,3 @@
 export const URL = "https://gravitel-graphql-backend.herokuapp.com/graphql";
+export const routeUrl: string =
+  process.env.NODE_ENV === "production" ? "/pie-charts-app" : "";
